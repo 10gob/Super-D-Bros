316442b421cc10b13921cba03004ac92754f532a
10gob <49033599+10gob@users.noreply.github.com>
2019-04-15 20:16:26 +0200

    Creación de GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Al crear un script y darle de nombre GameManager, aparece automaticamente el icono de un engranaje en el script
+//este script se lo damos a un objeto vacio (al que hemos llamado Game Manager) que hemos creado en la scene. Es recomendable
+//dar una etiqueta de color y colocarlo en el (0,0), o en alguna otra posicion en la que lo encontremos facilmente.
+
+
+//Posibles estados del videojuego
+public enum GameState             
+{
+    menu, inGame, gameOver
+}
+
+public class GameManager : MonoBehaviour
+{   
+    //Variable que referencia al propio gamemanager
+    public static GameManager sharedInstance;        //Singleton. De esta manera conseguimos que solo exista un GameManager
+
+    public GameState currentStateGame = GameState.menu;
+
+    private void Awake()
+    {
+        sharedInstance = this;
+    }
+
+    private void Start()
+    {
+        BackToMenu();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Start"))     //esta forma de configurar los botones se utiliza para juegos multiplataforma. Lo que hacemos es crear un boton llamado "Start"
+        {                                     //que posteriormente podremos asignar a una tecla del teclado, un boton de nintendo switch, etc. 
+            StartGame();                      // para configurar este boton debemos irnos a edit-->project settings-->input y crear un nuevo boton. Ej: si pone 18 escribimos 19
+        }
+    }
+
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+    }
+
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    void SetGameState(GameState newGameState)    //este metodo sirve para cambiar el estado del juego
+    {
+        if(newGameState == GameState.menu)
+        {
+            //hay que preparar la escena de unity para mostrar el menu
+        }
+
+        if(newGameState == GameState.inGame)
+        {
+            //hay que prepara la escena de unity para jugar
+        }
+
+        if(newGameState == GameState.gameOver)
+        {
+            //hay que preparar la escena de unity para el game over
+        }
+
+        //asignamos el estado de juego actual al que nos ha llegado por parametros
+        this.currentStateGame = newGameState;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Para definir el metodo jump vamos a necesitar dos variables: jumpforce y el rigidbody del personaje
+    public float jumpForce = 55f;
+    private Rigidbody2D rigidbody;
+
+    public LayerMask ground;        /*layer mask sirve para crear una capa. Esto es necesario para el salto. 
+                                     *Ademas debemos crear en unity una nueva capa llamada ground y aplicarsela a todos los suelos
+                                     *Lo mejor es aplicarla al prefab para asi tenerla en todos los hijos
+                                     * Luego en el script del jugador debemos seleccionar la capa ground
+                                     */
+
+    public Animator animation;           //creamos esta variable para la animacion del personaje
+                                         //en unity tenemos que arrastrar el sprite del jugador a la casilla de animator que nos ha aparecido en el inspector.
+    public float runningSpeed = 2.1f;     
+
+    //Antes de que el juego comience, necesitamos que la variable rigidbody tome el rigidbody del personaje. 
+    //Si lo hicieramos en el start, podrian pasar algunos segundos antes de que el rigidbody se configurara. 
+    //Todo lo que suceda en el awake esta listo antes de empezar a ejecutarse el juego. 
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>(); //Get component sirve para recorrer el inspector, viendo todas las componentes
+                                                 //que tiene un objeto, en este caso el player, para tomar la componente deseada.
+                                                 //En este caso, el rigidbody.
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation.SetBool("isAlive", true);          //hacemos que al iniciarse el videojuego, las variables boleanas tocar el suelo y estar vivo sean verdaderas
+        animation.SetBool("isGrounded", true);       //Set bool, set float, etc atribuye el valor que queramos a una variable.
+    }
+
+    // Update is called once per frame. El update es llamado hasta 60 veces por segundo. 
+    void Update() 
+    {
+        if (GameManager.sharedInstance.currentStateGame == GameState.inGame)  //con esta linea conseguimos que el personaje no se mueva mientras estamos en los menus o en el game over
+        {                                                                     //Solo nos movemos durante el inGame
+            if (IsTouchingTheGround())
+            {
+                if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) //detectamos si el jugador pulsa la tecla espacio para saltar y si esta en el suelo                                                         
+                {                                      //click izquierdo del raton
+                    Jump();      //La idea es que en el star, en el update y en el awake, el codigo este lo mas limpio posible.
+                }
+            }
+
+            animation.SetBool("isGrounded", IsTouchingTheGround());   //con esta linea comprobamos si el jugador esta tocando el suelo en cada frame
+                                                                      //Para ello utilizamos el metodo ya creado IsTouchingTheGround
+        }
+
+    }
+
+    void FixedUpdate()     //los movimientos como avanzar y retroceder los definimos siempre en el fixedupdate
+    {
+        if (GameManager.sharedInstance.currentStateGame == GameState.inGame)  
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                rigidbody.velocity = new Vector2(runningSpeed, rigidbody.velocity.y);  //el vector velocidad del rigidbody sera: (runningSpeed, la velocidad que tuviera en y )
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                rigidbody.velocity = new Vector2(-runningSpeed, rigidbody.velocity.y);
+            }
+        }
+            
+    }
+
+    /*SOBRE EL MOVIMIENTO Y SU IMPLEMENTACION 
+     * Input.GetKeyDown (up, etc) detecta si pulsamos la tecla. En el caso afirmativo, ejecuta la orden una unica vez.
+     * Input.GetKey (up, etc) detecta si la tecla se mantiene pulsada, haciendo que la orden se ejecute de manera continua. 
+     */
+
+    void Jump()
+    {
+        rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            //AddForce sirve para aplicar una fuerza. Vector2.up es un vector en 2d que apunta hacia arriba. 
+            //ForceMode2d sirve para aplicar una fuerza constante o un impulso. En el caso de un salto, elegimos impulso. 
+
+            //Como F=m*a, la aceleracion del salto sera a=F/m. La aceleración del salto dependera de la jumpforce aplicada y de 
+            //la masa del rigidbody. Podemos balancear masa y fuerza a mano o usando el checkbox "use auto mass". Esta opcion establece 
+            //una masa para el rigidbody en función del tamaño de este.
+    }
+
+    bool IsTouchingTheGround()             //este metodo detecta si el jugador esta en el suelo
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 0.2f, ground))                                                                  
+        {
+            return true;
+        }
+        else
+        {
+            return false; 
+        }
+        
+        /*physics2D.raycast               sirve para trazar un rayo
+         *this.transform.position         desde la posicion del jugador
+         * vector2.down                   apuntando hacia abajo
+         * 0.2f:                          a una distancia maxima de 0.2 metros
+         * ground:                        indica que nos hemos encontrado con la capa suelo. 
+         */
+    }
+}
